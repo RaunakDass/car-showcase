8086de6e621cfc3d0aa287ca322a055a8c3a35ac
Raunak Das <dasraunak5@gmail.com>
2024-04-23 09:34:32 +0530

    Added Search Bar

---
+++ components/SearchBar.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import Image from "next/image";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+
+//import SearchManufacturer from "./SearchManufacturer";
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src={"/magnifying-glass.svg"}
+      alt={"magnifying glass"}
+      width={40}
+      height={40}
+      className='object-contain'
+    />
+  </button>
+);
