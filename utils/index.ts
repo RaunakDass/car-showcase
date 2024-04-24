30c7208c8733dcec10ee50559e7b7f6d52e12272
Raunak Das <dasraunak5@gmail.com>
2024-04-24 17:05:51 +0530

    Changes to utils

--- utils/index.ts
+++ utils/index.ts
@@ -1,16 +1,14 @@
-const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '6385c50d4emsha0af529b8ee8f54p169eefjsn876eb5dd561e',
-		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-	}
-};
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
+
+export async function fetchCars(){
+    const headers = {
+        'X-RapidAPI-Key': '6385c50d4emsha0af529b8ee8f54p169eefjsn876eb5dd561e',
+		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+    }
+
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars', {
+        headers: headers,
+     });
+
+     const result = await response.json()
 }
