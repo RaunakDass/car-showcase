84f0f5c49a7950dfd4f2ae38008e9263405a3068
Raunak Das <dasraunak5@gmail.com>
2024-04-27 12:06:56 +0530

    Changes to utils

--- utils/index.ts
+++ utils/index.ts
@@ -1,14 +1,19 @@
 import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars(){
+export async function fetchCars(filters: FilterProps){
+    const { manufacturer, year, model, limit, fuel } = filters;
+
     const headers = {
         'X-RapidAPI-Key': '6385c50d4emsha0af529b8ee8f54p169eefjsn876eb5dd561e',
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
 
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars', {
+    const response = await fetch(
+      `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
+      {
         headers: headers,
-     });
+      }
+    );
 
      const result = await response.json()
 }
