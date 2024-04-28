9fcdf66f75df1c9375be8be30e8e339586e1ef7c
Raunak Das <dasraunak5@gmail.com>
2024-04-28 09:49:20 +0530

    All changes done

--- app/page.tsx
+++ app/page.tsx
@@ -1,58 +1,79 @@
-import { fetchCars } from "@/utils";
-import { HomeProps } from "@/types";
-import { fuels, yearsOfProduction } from "@/constants";
-import { CarCard, ShowMore, SearchBar, CustomFilter, Hero } from "@/components";
 
-export default async function Home({ searchParams }: HomeProps) {
+import { Hero, CustomFilter, SearchBar, CarCard, ShowMore } from "@/components";
+import { fetchCars } from "@/utils";
+import { fuels, yearsOfProduction } from "@/constants";
+import { FilterProps } from "@/types";
+
+const CARS_PER_PAGE = 10; // cars per page
+
+// searchParams interface
+interface searchParamsProps {
+  searchParams: FilterProps;
+}
+
+// home
+export default async function Home({ searchParams }: searchParamsProps) {
+  // fetch all cars from api
   const allCars = await fetchCars({
     manufacturer: searchParams.manufacturer || "",
-    year: searchParams.year || 2022,
+    year: searchParams.year || new Date().getFullYear(),
     fuel: searchParams.fuel || "",
-    limit: searchParams.limit || 10,
+    limit: searchParams.limit || CARS_PER_PAGE,
     model: searchParams.model || "",
   });
 
+  // is car data empty
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
   return (
-    <main className='overflow-hidden'>
+    <div className="overflow-hidden">
+      {/* hero */}
       <Hero />
 
-      <div className='mt-12 padding-x padding-y max-width' id='discover'>
-        <div className='home__text-container'>
-          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
-          <p>Explore out cars you might like</p>
+      {/* discover */}
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          {/* heading and subheading */}
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
         </div>
 
-        <div className='home__filters'>
+        {/* filters */}
+        <div className="home__filters">
+          {/* search bar */}
           <SearchBar />
 
-          <div className='home__filter-container'>
-            <CustomFilter title='fuel' options={fuels} />
-            <CustomFilter title='year' options={yearsOfProduction} />
+          {/* custom filters */}
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" options={fuels} />
+            <CustomFilter title="year" options={yearsOfProduction} />
           </div>
         </div>
 
+        {/* check if car data is empty */}
         {!isDataEmpty ? (
           <section>
-            <div className='home__cars-wrapper'>
-              {allCars?.map((car) => (
-                <CarCard car={car} />
+            {/* show all cars */}
+            <div className="home__cars-wrapper">
+              {allCars?.map((car: any, i) => (
+                <CarCard car={car} key={`car-${i}`} />
               ))}
             </div>
 
+            {/* show more button */}
             <ShowMore
-              pageNumber={(searchParams.limit || 10) / 10}
-              isNext={(searchParams.limit || 10) > allCars.length}
+              pageNumber={(searchParams.limit || CARS_PER_PAGE) / CARS_PER_PAGE}
+              isNext={(searchParams.limit || CARS_PER_PAGE) > allCars.length}
             />
           </section>
         ) : (
-          <div className='home__error-container'>
-            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
+          // No results found
+          <div className="home__error-container">
+            <h2 className="text-black text-xl text-bold">Oops, no results</h2>
             <p>{allCars?.message}</p>
           </div>
         )}
       </div>
-    </main>
+    </div>
   );
 }
--- utils/index.ts
+++ utils/index.ts
@@ -5,7 +5,7 @@ export async function fetchCars(filters: FilterProps){
 
     const headers = {
         'X-RapidAPI-Key': '6385c50d4emsha0af529b8ee8f54p169eefjsn876eb5dd561e',
-		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+		    'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
 
     const response = await fetch(
@@ -37,7 +37,7 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
     const url = new URL("https://cdn.imagin.studio/getimage");
     const { make, model, year } = car;
   
-    url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
+    url.searchParams.append('customer', '6385c50d4emsha0af529b8ee8f54p169eefjsn876eb5dd561e' || '');
     url.searchParams.append('make', make);
     url.searchParams.append('modelFamily', model.split(" ")[0]);
     url.searchParams.append('zoomType', 'fullscreen');
